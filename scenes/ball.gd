a962079f48764de754cf590050c9cfe5af8f8058
PuntitOwO <christopher.marin@ug.uchile.cl>
2025-06-17 23:31:32 -0400

    feat: stretch goals

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -7,6 +7,8 @@ const INITIAL_SPEED := 200
 var speed := 200
 var acceleration_delta := 8
 
+@onready var collision_sfx: AudioStreamPlayer = $CollisionSFX
+
 func _ready() -> void:
 	compute_initial_velocity()
 
@@ -18,6 +20,7 @@ func _physics_process(delta: float) -> void:
 	velocity = velocity.normalized() * speed
 	var collision := move_and_collide(velocity * delta)
 	if collision == null: return
+	collision_sfx.play()
 	velocity = velocity.bounce(collision.get_normal())
 	var collider := collision.get_collider() as Node2D
 	if collider.is_in_group("Block"):
--- scenes/main.gd
+++ scenes/main.gd
@@ -12,11 +12,17 @@ static var high_score := 0
 	$UI/MarginContainer/MarginContainer/VBoxContainer/LifeIcon2,
 	$UI/MarginContainer/MarginContainer/VBoxContainer/LifeIcon3
 ]
+@onready var miss_sfx: AudioStreamPlayer = $MissSFX
+@onready var win_sfx: AudioStreamPlayer = $WinSFX
+@onready var level: TileMapLayer = $Level
+var level_template: PackedByteArray
 
 func _ready() -> void:
 	ball.brick_broken.connect(_add_score)
 	dead_zone.body_entered.connect(_lose_life)
 	%HighScore.text = str(high_score)
+	_randomize_tile_color.call_deferred()
+	level_template = level.tile_map_data
 
 func _add_score() -> void:
 	score += 100
@@ -24,11 +30,26 @@ func _add_score() -> void:
 	if high_score < score:
 		high_score = score
 		%HighScore.text = str(high_score)
+	if get_tree().get_node_count_in_group("Block") <= 1:
+		win_sfx.play()
+		level.set_deferred("tile_map_data", level_template)
+		await get_tree().physics_frame
+		_randomize_tile_color.call_deferred()
+
+func _randomize_tile_color() -> void:
+	var colors : Array[Color] = []
+	for i in 7: colors.append(Color(randf(),randf(),randf()))
+	for node in get_tree().get_nodes_in_group("Block"):
+		var block := node as Node2D
+		var y := block.global_position.y - 120
+		var idx := int(y / 48)
+		block.modulate = colors[idx]
 
 func _lose_life(_body: Node2D) -> void:
 	lives -= 1
 	life_icons[lives].hide()
 	if lives > 0:
 		paddle.reset()
+		miss_sfx.play()
 	else:
 		get_tree().reload_current_scene.call_deferred()
